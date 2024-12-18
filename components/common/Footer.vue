c3cd71f50194dbda6e9e7935e42a48d0ea0c711c
KimJunGyeom <155404741+GitHubKimJunGyeom@users.noreply.github.com>
2024-12-18 17:19:23 +0900

    일단 기본 헤더 푸터 제작

---
+++ components/common/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <footer>
+        <v-row>
+            <v-col>
+                이것은 픽셀삼국 프론트 푸터 컴포넌트입니다.
+            </v-col>
+        </v-row>
+    </footer>
+</template>
---
+++ components/common/Header.vue
@@ -0,0 +1,9 @@
+<template>
+    <header>
+        <v-row>
+            <v-col>
+                이것은 픽셀삼국 프론트 헤더 컴포넌트입니다.
+            </v-col>
+        </v-row>
+    </header>
+</template>
