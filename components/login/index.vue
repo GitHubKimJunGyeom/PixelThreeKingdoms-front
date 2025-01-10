40fcd5e2ccf12efddde7110e2f7d3a5a689ddc43
KimJunGyeom <155404741+GitHubKimJunGyeom@users.noreply.github.com>
2025-01-10 16:26:14 +0900

    로그인 화면 및 각종 기본 자바스크립트 더미 작성

---
+++ components/login/index.vue
@@ -0,0 +1,84 @@
+<template>
+    <v-app>
+      <v-container>
+        <v-row justify="center">
+          <v-col cols="12" sm="8" md="6">
+            <v-card class="elevation-12">
+              <v-toolbar color="primary" dark flat>
+                <v-toolbar-title>로그인</v-toolbar-title>
+              </v-toolbar>
+  
+              <v-card-text>
+                <div id="GgCustomLogin" class="text-center">
+                  <v-btn color="red" dark @click="onGoogleLogin">
+                    <v-icon left>mdi-google</v-icon>
+                    Google로 로그인하기
+                  </v-btn>
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-app>
+  </template>
+  
+  <script>
+  export default {
+    mounted() {
+      this.initGoogleAuth();
+    },
+    methods: {
+      initGoogleAuth() {
+        gapi.load("auth2", () => {
+          const auth2 = gapi.auth2.init({
+            client_id: "217755353555-3msma49ckq4fa47k0tg5tkshatcdh8c9.apps.googleusercontent.com",
+            scope: "email profile openid https://www.googleapis.com/auth/user.birthday.read",
+          });
+          this.authInstance = auth2;
+        });
+      },
+      onGoogleLogin() {
+        if (!this.authInstance) {
+          console.error("Google Auth Instance not initialized.");
+          return;
+        }
+        this.authInstance.signIn().then(
+          (googleUser) => {
+            const accessToken = googleUser.getAuthResponse().access_token;
+            const profile = googleUser.getBasicProfile();
+            console.log("User Profile:", profile);
+  
+            // Google People API 요청
+            this.$axios
+              .$get("https://people.googleapis.com/v1/people/me", {
+                params: {
+                  personFields: "birthdays",
+                  key: "AIzaSyBOdmeC4SOSzXmPGLEM2vZueqiBSWKg3wk",
+                  access_token: accessToken,
+                },
+              })
+              .then((response) => {
+                console.log("People API Response:", response);
+              })
+              .catch((error) => {
+                console.error("People API Error:", error);
+              });
+          },
+          (error) => {
+            console.error("Google Sign-In Error:", error);
+          }
+        );
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  #GgCustomLogin {
+    margin-top: 20px;
+  }
+  </style>
+  
+  <script src="https://apis.google.com/js/platform.js" async defer></script>
+  
---
+++ public/js/const.js
@@ -0,0 +1,36 @@
+export const BOARD_LIST = {
+    patchNote,
+    programing,
+    guestBook,
+}
+
+
+export const TEXT_INFO = {
+    KO: {
+      PAGE_TITLE: '픽셀 삼국지',
+      WELCOME_MESSAGE: '환영합니다!',
+      START_GAME: '게임 시작',
+      SETTINGS: '설정',
+      PATCH_NOTE_NAME: '패치노트',
+      PROGRAMING_NAME: '프로그래밍노트',
+      GUEST_BOOK_NAME: '방명록',
+    },
+    EN: {
+      PAGE_TITLE: 'Pixel Three Kingdoms',
+      WELCOME_MESSAGE: 'Welcome!',
+      START_GAME: 'Start Game',
+      SETTINGS: 'Settings',
+      PATCH_NOTE_NAME: 'patch note',
+      PROGRAMING_NAME: 'programing',
+      GUEST_BOOK_NAME: 'guest book',
+    },
+    JA: {
+      PAGE_TITLE: 'ピクセル三国志',
+      WELCOME_MESSAGE: 'ようこそ!',
+      START_GAME: 'ゲームを始める',
+      SETTINGS: '設定',
+      PATCH_NOTE_NAME: 'パッチノート',
+      PROGRAMING_NAME: 'プログラミングノート',
+      GUEST_BOOK_NAME: '芳名録',
+    },
+  };  
